faa9abddc64fb202bccf91b008d7bbdf7e3066cf
Evan Dawson <Zormion@gmail.com>
2014-08-19 16:26:28 -0400

    I added a game

---
+++ Project Unicorn/src/pack/Main/Game.java
@@ -0,0 +1,22 @@
+package pack.Main;
+
+
+public class Game implements Runnable{
+	
+	private boolean isRunning = true;
+	
+	public static void main(String args[]) {
+		Window window = new Window(800, 600, "Unicorn Clicker v1.0");
+	}
+	
+	public void run() {
+		while(isRunning == true) {
+			
+		}
+	}
+	
+	
+	
+}
+
+
---
+++ Project Unicorn/src/pack/Main/Window.java
@@ -0,0 +1,27 @@
+package pack.Main;
+
+import javax.swing.JFrame;
+
+public class Window {
+	JFrame frame;
+	public Window(int width, int height, String title) {
+		frame = new JFrame(title);
+		frame.setSize(width, height);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+	
+	public void resize(int width, int height) {
+		frame.setSize(width, height);
+	}
+	
+	public void rename(String name) {
+		frame.setName(name);
+	}
+	
+	public void undecorate(boolean undecorate) {
+		frame
+	}
+}
