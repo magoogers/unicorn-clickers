b828f4634f664934371fd4937e8bc6ec2e3a72ae
TheLetterSix <lmaclean234@gmail.com>
2014-08-20 16:27:57 -0400

    made menu state

--- Project Unicorn/src/pack/main/Game.java
+++ Project Unicorn/src/pack/main/Game.java
@@ -1,5 +1,7 @@
 package pack.main;
 
+import org.newdawn.slick.state.BasicGameState;
+
 
 
 public class Game extends BasicGameState{
---
+++ Project Unicorn/src/pack/main/states/Menu.java
@@ -0,0 +1,30 @@
+package pack.main.states;
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Menu extends BasicGame{
+
+	public Menu(String title) {
+		super(title);
+		
+	}
+
+	public void init(GameContainer gc) throws SlickException {
+		
+		
+	}
+	
+	public void render(GameContainer gc, Graphics g) throws SlickException {
+		
+		
+	}
+	
+	public void update(GameContainer gc, int delta) throws SlickException {
+		
+		
+	}
+
+}
