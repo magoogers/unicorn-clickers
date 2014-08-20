a4c2bd691723d480b191aad33d11ac58ae52a1bc
Evan Dawson <Zormion@gmail.com>
2014-08-20 14:43:46 -0400

    recreate main - cuase butthead wanted to...

--- Project Unicorn/src/pack/main/Main.java
+++ Project Unicorn/src/pack/main/Main.java
@@ -1,48 +1,21 @@
 package pack.main;
 
-import java.awt.Graphics;
-
-//import pack.main.game.Menu;
-import pack.main.input.Input;
-//import pack.main.states.StateManager;
-
-
 public class Main implements Runnable{
 	
 	private boolean isRunning = true;
-	static Graphics g = null;
+	
 	static Window window;
-	static Input i;
 	
 	public static void main(String args[]) {
 		window = new Window(800, 600, "Unicorn Clicker v1.0");
 		
-		i = new Input();
-		
-		
 	}
 	
 	public void run() {
-		//if(sm.getState() == menu.getID()){
-
-		//g.drawString("hello",100,100);
-			//menu.init(window);
-		//}
-		while(window.getRunning()) {
-			//if(sm.getState() == menu.getID()){
-				//menu.render(window,sm);
-				//menu.update(window, i,sm);
-				
-			//}
+		while(true) {
+			
 		}
-		
-		
-		
-		
 	}
-	
-	
-	
 	
 }
 
