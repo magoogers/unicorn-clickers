89c2b151f4b41c0b6baf807ea6b6928789802e06
Evan Dawson <Zormion@gmail.com>
2014-08-20 16:34:26 -0400

    g

--- Project Unicorn/src/pack/Menu.java
+++ Project Unicorn/src/pack/Menu.java
@@ -1,30 +1,38 @@
 package pack;
 
-import org.newdawn.slick.BasicGame;
+
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
 
-public class Menu extends BasicGame{
+public class Menu extends BasicGameState{
 
-	public Menu(String title) {
-		super(title);
-		
-	}
-
-	public void init(GameContainer gc) throws SlickException {
-		
-		
-	}
 	
-	public void render(GameContainer gc, Graphics g) throws SlickException {
+	public void init(GameContainer arg0, StateBasedGame arg1) throws SlickException {
 		
 		
 	}
+
 	
-	public void update(GameContainer gc, int delta) throws SlickException {
-		
+	public void render(GameContainer arg0, StateBasedGame arg1, Graphics arg2) throws SlickException {
 		
 	}
+
+	@Override
+	public void update(GameContainer arg0, StateBasedGame arg1, int arg2)
+			throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
 
 }
