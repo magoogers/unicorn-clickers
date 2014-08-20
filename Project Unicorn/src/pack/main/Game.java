1194ac758bc8eb252445586561d94eb8f1d55c97
Evan Dawson <Zormion@gmail.com>
2014-08-20 16:12:14 -0400

    Removed all, added slick

--- Project Unicorn/src/pack/main/Game.java
+++ Project Unicorn/src/pack/main/Game.java
@@ -1,14 +1,5 @@
 package pack.main;
 
-public class Game implements Runnable{
-	
-	public static void main(String args[]) {
-		Window window = new Window(800, 600, "Project Unicorn v1");
-	}
+public class Game {
 
-	public void run() {
-		
-		
-	}
-	
 }
